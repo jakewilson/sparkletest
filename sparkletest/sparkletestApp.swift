598ae93135cf793a15833d396e9b92c25a296cdb
Jake Wilson <jake@jakewilson.org>
2024-10-14 18:11:15 -0700

    Initial Commit

---
+++ sparkletest/sparkletestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkletestApp.swift
+//  sparkletest
+//
+//  Created by Jake Wilson on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct sparkletestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sparkletestTests/sparkletestTests.swift
@@ -0,0 +1,17 @@
+//
+//  sparkletestTests.swift
+//  sparkletestTests
+//
+//  Created by Jake Wilson on 10/14/24.
+//
+
+import Testing
+@testable import sparkletest
+
+struct sparkletestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
